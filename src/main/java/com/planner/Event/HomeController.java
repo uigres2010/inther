5aa34758a4b2a97b3bdd87cb3bb7151baa3d2083
Sergiu <uigres2010@gmail.com>
2019-07-09 10:12:51 +0300

    start

---
+++ src/main/java/com/planner/Event/HomeController.java
@@ -0,0 +1,15 @@
+package com.planner.Event;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "Hello Event planner";
+
+    }
+}
